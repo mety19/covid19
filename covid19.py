00236a357769a5cd206161e38922c2d2656be71c
mety19 <8251930+mety19@users.noreply.github.com>
2020-04-14 14:17:14 -0500

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -2,10 +2,7 @@
 import dash
 #import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-
-
-''' APP '''
+#import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -37,6 +34,5 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 ])
 
 
-
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
