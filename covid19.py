1531319bcf6b6e28c8d9e67a7d9d5f2b23df754b
mety19 <8251930+mety19@users.noreply.github.com>
2020-11-04 13:23:52 -0600

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -3,12 +3,16 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import numpy as np
-#import requests
+import datetime as dt
 
 ''' PREPARE WORLD DATA '''
 # Read data from https://covidtracking.com/ 
 urlworld = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
 world = pd.read_csv(urlworld)
+
+# Remove US terriories from world data
+usterritories = ['American Samoa', 'Guam', 'Northern Mariana Islands', 'Puerto Rico', 'United States Virgin Islands']
+world = world[~world['location'].isin(usterritories)]
 
 # Select World Fields and Rename columns and sort data
 worldfields = ['iso_code', 'location',	'date',	'total_cases', 'new_cases',	'total_deaths',	'new_deaths'
@@ -47,21 +51,18 @@ world = pd.DataFrame(world[['dateChecked', 'area', 'states', 'positive', 'negati
 urlus = 'https://covidtracking.com/api/v1/us/daily.csv'
 urlst = 'https://covidtracking.com/api/v1/states/daily.csv'
 covus = pd.read_csv(urlus)
-covus.loc[covus['states']>1,'states'] = 'US'
+covus.loc[covus['states']>1,'states'] = 'USA'
+covst = pd.read_csv(urlst)
+covst = covst[covst.dateChecked.notnull()]
+covst.state = 'USA'+', '+covst.state
 
 # Read us and world population data
 urluspop = 'https://github.com/mety19/covid19/raw/master/uspopulation.csv'
-covst = pd.read_csv(urlst)
 uspop = pd.read_csv(urluspop)
 uspop = uspop.iloc[:, 0:2]
 uspop.columns =['states', 'population']
+uspop.states = 'USA'+', '+uspop.states
 
-#urlworldpop = 'https://www.worldometers.info/world-population/population-by-country/'
-#worldhtml = requests.get(urlworldpop).content
-#worldpop_list = pd.read_html(worldhtml)
-#worldpop = pd.DataFrame(worldpop_list[-1])
-#worldpop = worldpop.iloc[:, 1:3]
-#worldpop.columns = ['states', 'population']
 urlworldpop = 'https://github.com/mety19/covid19/raw/master/worldpopulation.csv'
 worldpop = pd.read_csv(urlworldpop)
 worldpop.columns = ['states', 'population']
@@ -85,12 +86,16 @@ covst.columns = ['dateChecked', 'area', 'states', 'positive', 'negative', 'hospi
                             , 'totalTestResults', 'fips', 'deathIncrease', 'hospitalizedIncrease'
                             , 'negativeIncrease', 'positiveIncrease', 'totalTestResultsIncrease']
 
+# Get US state list and combined jurisdictions
+statesall = covst['states'].unique()
+statescountryall = np.concatenate([statesall, countryall])
 
 ''' COMBINE WORLD AND US DATA'''
-# Append the two dataframes and make date a datatime type 
+# Append the two dataframes and make date, and dateindex datatime types 
 world = world.fillna(1)
 worldusa = world.append([covus, covst], sort=False)
-worldusa['Date'] = pd.to_datetime(worldusa['dateChecked'])
+worldusa['dateChecked'] = worldusa.dateChecked.str.split('T').str[0]
+worldusa['Date'] = worldusa['Dateindex'] = pd.to_datetime(worldusa['dateChecked'])
 
 # Merge with population data and get state list
 worldusa = pd.merge(worldusa, pop, on='states')
@@ -98,14 +103,16 @@ worldusa = pd.merge(worldusa, pop, on='states')
 # Get US state list
 statesall = worldusa[worldusa.area == 'USA']['states'].unique()
 
-# Create dictionary of US states and world country list
+# Create dictionary of US states and world country lists
 all_options = {'USA': statesall
-               , 'World': countryall}
+               , 'World': countryall
+               , 'All': statescountryall}
 covall = worldusa
 
 # cumulative and incremental columns
-Cumulative = ['Date', 'states', 'totalTestResults', 'positive', 'hospitalized', 'death', 'total', 'population']
-Incremental = ['Date', 'states', 'totalTestResultsIncrease', 'positiveIncrease', 'hospitalizedIncrease', 'deathIncrease', 'total', 'population']
+Cumulative = ['Date', 'states', 'totalTestResults', 'positive', 'hospitalized', 'death', 'total', 'population', 'Dateindex', 'dateChecked']
+Incremental = ['Date', 'states', 'totalTestResultsIncrease', 'positiveIncrease', 'hospitalizedIncrease', 'deathIncrease', 'total', 'population', 'Dateindex', 'dateChecked']
+
 
 ''' APP '''
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -123,7 +130,7 @@ colors = {
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.Div([
             
-        html.H1(children='COVID-TRACK: World and USA SARS-COV-2 Testing and COVID-19 Tracking', style={
+        html.H1(children='World and USA SARS-COV-2 Testing and COVID-19 Tracking', style={
             'textAlign': 'center',
             'color': colors['text']
             })    
@@ -134,9 +141,15 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
      html.Div([
         html.Div([
                 html.Div([
-                    html.P('This app plots data referenced in covidtracking.com and covid.ourworldindata.org. These datasets have been put together to attempt to aggregate covid testing data in the most accurate way,  and are not necessarily complete in terms of number of tests, cases, hospitalizations or deaths.'
+                    html.P('''This app plots data referenced in covidtracking.com and covid.ourworldindata.org. 
+                           These datasets have been put together to attempt to aggregate COVID-19 related data in 
+                           the most accurate way and are not necessarily complete in terms of number of tests, cases, hospitalizations, 
+                           or deaths. We selected epidemiology variables related to testing, incidence, hospitalizations, and death since 
+                           incidence data is difficult to interpret relative to growth unless we understand the testing rate. 
+                           In addition, not all cases are equally severe in all places. Hospitalizations and death data convey information 
+                           about severe cases when data is available.'''
                            )
-                        ], style = {'color': colors['text'], 'textAlign': 'center', 'size': 16}, className = "row"),
+                        ], style = {'color': colors['text'], 'textAlign': 'center', 'size': 14}, className = "row"),
 
                 html.Div([
                         html.A("Data Source: https://covidtracking.com"
@@ -161,7 +174,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                             {'label': k, 'value': k} for k in all_options.keys()
                             ],
                     style={'backgroundcolor': '#030A32', 'color': '#FEFCFC'},
-                    value = 'USA'
+                    value = 'All'
                     )
                 ], className = 'one columns', style = {'margin-top': '20'}),     
 
@@ -173,7 +186,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 #                        {'label': k, 'value': k} for k in all_options.keys()
 #                        ]
 #                    ,
-                    value=['US'],
+                    value=['United States'],
                     multi=True
                     )  
                 ],
@@ -199,7 +212,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
             )
        ,
         html.Div([
-            html.P('Aggregation:', style = {'backgroundcolor': '#030A32', 'color': '#FEFCFC'}),
+            html.P('Smoothing:', style = {'backgroundcolor': '#030A32', 'color': '#FEFCFC'}),
             dcc.RadioItems(
                     id = 'MovingAverage',
                     options=[
@@ -232,6 +245,21 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         ], className="row"
     ),    
         
+     html.Div([ 
+       html.Div([
+           html.P('')
+           ], className='four columns'),
+       html.Div([
+           dcc.DatePickerRange(
+           id = 'DateChoice',
+           start_date = dt.datetime(2019, 12, 31),
+           end_date = dt.datetime(2020, 7, 31)
+           )], className='four columns', style={'text-align': 'center'}),
+       html.Div([
+           html.P('')
+           ], className='four columns')
+       ], className="row"
+    ), 
       
              
     html.Div([
@@ -340,8 +368,10 @@ def set_scope_option(selected_scope):
         , dash.dependencies.Input('CumulIncr', 'value')
         , dash.dependencies.Input('Scale', 'value')
         , dash.dependencies.Input('MovingAverage', 'value')
+        , dash.dependencies.Input('DateChoice', 'start_date')
+        , dash.dependencies.Input('DateChoice', 'end_date')
         ])
-def update_graph_src(statesel, cumulincr, scale, movingaverage):
+def update_graph_src(statesel, cumulincr, scale, movingaverage, start_date, end_date):
     data = []
     if cumulincr=='Cumulative':
         covsel = covall[Cumulative]
@@ -365,7 +395,11 @@ def update_graph_src(statesel, cumulincr, scale, movingaverage):
     if scale =='Raw':
         covsel = covsel
     elif scale=='Log10':
-        covsel.iloc[:,2:7] = np.log10(covsel.iloc[:,2:7])
+        covsel.iloc[:,2:7] = np.log10(covsel.iloc[:,2:7])      
+        
+    covsel = covsel.sort_values(by='Dateindex')
+    covsel.set_index('Dateindex', inplace=True)
+    covsel = covsel.loc[start_date:end_date]
         
     for state in statesel:
         covstate = pd.DataFrame(covsel.loc[covsel['states'] == state])
@@ -418,8 +452,10 @@ def update_graph_src(statesel, cumulincr, scale, movingaverage):
         , dash.dependencies.Input('CumulIncr', 'value')
         , dash.dependencies.Input('Scale', 'value')
         , dash.dependencies.Input('MovingAverage', 'value')
+        , dash.dependencies.Input('DateChoice', 'start_date')
+        , dash.dependencies.Input('DateChoice', 'end_date')
         ])
-def update_graph_src(statesel, cumulincr, scale, movingaverage):
+def update_graph_src(statesel, cumulincr, scale, movingaverage, start_date, end_date):
     data = []
     if cumulincr=='Cumulative':
         covsel = covall[Cumulative]
@@ -436,16 +472,17 @@ def update_graph_src(statesel, cumulincr, scale, movingaverage):
         covsel = covall[Cumulative]
         covsel.iloc[:,2:7] = (covsel.iloc[:,2:7].div(covsel.total, axis=0))
         covsel = covsel.round(4)
-        #if covsel.iloc[:,2:7]>1:
-        #    covsel.iloc[:,2:7]=0
         covsel.iloc[:,2:7]=covsel.iloc[:,2:7].mask(covsel.iloc[:,2:7].gt(0.99),0)
         plottitle = 'Number of Positive Tests per Test'
         
     if scale =='Raw':
         covsel = covsel
     elif scale=='Log10':
-        covsel.iloc[:,2:7] = np.log10(covsel.iloc[:,2:7])
-    #    
+        covsel.iloc[:,2:7] = np.log10(covsel.iloc[:,2:7])      
+        
+    covsel = covsel.sort_values(by='Dateindex')
+    covsel.set_index('Dateindex', inplace=True)
+    covsel = covsel.loc[start_date:end_date]   
     
     for state in statesel:
         covstate = pd.DataFrame(covsel.loc[covsel['states'] == state])
@@ -498,8 +535,10 @@ def update_graph_src(statesel, cumulincr, scale, movingaverage):
         , dash.dependencies.Input('CumulIncr', 'value')
         , dash.dependencies.Input('Scale', 'value')
         , dash.dependencies.Input('MovingAverage', 'value')
+        , dash.dependencies.Input('DateChoice', 'start_date')
+        , dash.dependencies.Input('DateChoice', 'end_date')
         ])
-def update_graph_src(statesel, cumulincr, scale, movingaverage):
+def update_graph_src(statesel, cumulincr, scale, movingaverage, start_date, end_date):
     data = []
     if cumulincr=='Cumulative':
         covsel = covall[Cumulative]
@@ -521,7 +560,11 @@ def update_graph_src(statesel, cumulincr, scale, movingaverage):
     if scale =='Raw':
         covsel = covsel
     elif scale=='Log10':
-        covsel.iloc[:,2:7] = np.log10(covsel.iloc[:,2:7])
+        covsel.iloc[:,2:7] = np.log10(covsel.iloc[:,2:7])      
+        
+    covsel = covsel.sort_values(by='Dateindex')
+    covsel.set_index('Dateindex', inplace=True)
+    covsel = covsel.loc[start_date:end_date]
         
     for state in statesel:
         covstate = pd.DataFrame(covsel.loc[covsel['states'] == state])
@@ -574,8 +617,10 @@ def update_graph_src(statesel, cumulincr, scale, movingaverage):
         , dash.dependencies.Input('CumulIncr', 'value')
         , dash.dependencies.Input('Scale', 'value')
         , dash.dependencies.Input('MovingAverage', 'value')
+        , dash.dependencies.Input('DateChoice', 'start_date')
+        , dash.dependencies.Input('DateChoice', 'end_date')
         ])
-def update_graph_src(statesel, cumulincr, scale, movingaverage):
+def update_graph_src(statesel, cumulincr, scale, movingaverage, start_date, end_date):
     data = []
     if cumulincr=='Cumulative':
         covsel = covall[Cumulative]
@@ -597,7 +642,11 @@ def update_graph_src(statesel, cumulincr, scale, movingaverage):
     if scale =='Raw':
         covsel = covsel
     elif scale=='Log10':
-        covsel.iloc[:,2:7] = np.log10(covsel.iloc[:,2:7])
+        covsel.iloc[:,2:7] = np.log10(covsel.iloc[:,2:7])      
+        
+    covsel = covsel.sort_values(by='Dateindex')
+    covsel.set_index('Dateindex', inplace=True)
+    covsel = covsel.loc[start_date:end_date]
         
     for state in statesel:
         covstate = pd.DataFrame(covsel.loc[covsel['states'] == state])
